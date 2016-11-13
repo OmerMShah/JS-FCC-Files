b1d54ee3036265f2829930d59ab0b952d1a603c1
OmerMShah <omermshah@gmail.com>
2016-11-13 15:25:50 -0700

    testing push up to github

--- ConsoleLog.js
+++ ConsoleLog.js
@@ -1,9 +1,10 @@
 /**
- * Created by OmerShah on 2016-11-13.
+ * Created by OmerShah on 2016-11-13. Testing push, pull, and merge from WebStorm IDE. Testing push.
  */
 
 var myFirstName = "Omer";
 var myLastName = "Shah";
 var spaceChar = " ";
 var favAnimal = "Cat";
-console.log(myFirstName + spaceChar+  myLastName + spaceChar + "and my favorite animal is " + favAnimal);
+var favFood = "Pizza";
+console.log(myFirstName + spaceChar+  myLastName + spaceChar + "and my favorite animal is " + favAnimal + "and fav food is" + favFood);
